b59804fc219744256e3eedce241f2ee7232b3d43
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-06-07 16:36:43 +0200

    added function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 import imagesList from './gallery-items.js'
 
-
-
 const listGallery = document.querySelector('.js-gallery')
+const resultCreatImageElement = createImageElement(imagesList);
+
+
+listGallery.insertAdjacentHTML('beforeend', resultCreatImageElement)
+listGallery.addEventListener('click',onTargetImgClick)
+
+
+
+
+// function createImageElement(item) {
+//    return item.map(({preview,original}) => {
+//         return `
+//         <li class="gallery__item">
+//         <a
+//             class="gallery__link"
+//             href="${original}">
+//         <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//              alt="Tulips"
+//          />
+//         </a>
+//         </li>
+//         `
+//     }).join('')
+// }
 
 
 
@@ -11,21 +36,35 @@ function createImageElement(item) {
    return item.map(({preview,original}) => {
         return `
         <li class="gallery__item">
-        <a
-            class="gallery__link"
-            href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg">
+    
         <img
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
              alt="Tulips"
          />
-        </a>
+       
         </li>
         `
     }).join('')
 }
 
 
-const resultCreatImageElement = createImageElement(imagesList);
-listGallery.insertAdjacentHTML('beforeend', resultCreatImageElement)
+function onTargetImgClick(e) {
+    const elementTarget = e.target;
+    const IsImageElement = elementTarget.classList.contains('gallery__image');
+    const elementDataSource = elementTarget.dataset.source;
+
+    if (!IsImageElement) {
+        return 
+    }
+
+  console.log(elementTarget.dataset.source)
+}
+
+
+
+
+
+
+
